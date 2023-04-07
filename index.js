a949409a32a3d1636bf24e35a08ddc91d9a55ce8
abdullah <abdullahrecon999@gmail.com>
2023-04-07 19:16:09 +0500

    feat(index): added send image and buld mssg send

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const { create, Client ,ev} = require('@open-wa/wa-automate');
 const fs = require("fs");
+var fileupload = require("express-fileupload");
+const bodyParser = require('body-parser');
+const multer  = require('multer')
+const upload = multer({});
+
 // const qr = require('qr-image');
 
 // create a new Express app
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+// app.use(fileupload());
 
 // set up a new wa-automate-nodejs client
 clientSessions = {};
@@ -89,6 +99,51 @@ app.post('/sendmssg/:session', async (req, res) => {
   });
 })
 
+app.post('/sendImg', upload.single('image'),async (req, res) => {
+  const {caption, phonenumber, sessionId} = req.body;
+  // console.log(req.body)
+  // var caption="test"
+  // var phonenumber="923335662534@c.us"
+  // var sessionId="03115843266"
+  const imgUrl = "data:"+req.file.mimetype+";base64,"+req.file.buffer.toString('base64');
+  // //const imgUrl = "https://fastly.picsum.photos/id/791/536/354.jpg?hmac=eXUPs7QLTn9HF78YkhAz85vtEsUXj2aePprZoTCwCdU";
+
+  // if image is not present in the request, return an error
+  if (!req.file) {
+    return res.status(400).send('No image in the request');
+  } else {
+    // send the image to the WhatsApp chat
+    const client = clientSessions[sessionId]
+    client.sendImage(phonenumber+"@c.us", imgUrl, "testfile" + "@c.us", caption).then((result) => {
+      console.log("Result: ", result); //return object success
+      res.send("Image Sent")
+    }).catch((erro) => {
+      console.error("Error when sending: ", erro); //return object error
+      res.send("Image Not Sent")
+    });
+  }
+})
+
+app.post('/bulksend', async (req, res) => {
+  // phonenumbers => array of phone numbers
+  const {message, phonenumbers, sessionId} = req.body;
+  const client = clientSessions[sessionId]
+
+  await Promise.all(phonenumbers.map(async (number) => {
+    await client.sendText(number+"@c.us", message).then((result) => {
+      console.log("Result: ", result); //return object success
+    }).catch((erro) => {
+      console.error("Error when sending: ", erro); //return object error
+    });
+  })).then((vals) => {
+    res.send("Messages Sent")
+    console.log(vals)
+  }).catch(() => {
+    res.send("Messages Not Sent")
+  })
+
+})
+
 // set up an Express route to generate a QR code
 // app.get('/qrcode/:phoneNumber', async (req, res) => {
 //   // get the phone number from the request parameters
@@ -102,7 +157,5 @@ app.post('/sendmssg/:session', async (req, res) => {
 //   qr.image(qrCode, { type: 'png' }).pipe(res);
 // });
 
-// start the wa-automate-nodejs client
-
 // start the Express app
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3002, () => console.log('Server running on port 3000'));
